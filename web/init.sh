0e4dedb48beefbd5fb111ad9b130184f151804aa
Gepard45 <45Gepard@gmail.com>
2016-11-18 16:08:01 +0500

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,8 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
+
+sudo /etc/init.d/mysql start
